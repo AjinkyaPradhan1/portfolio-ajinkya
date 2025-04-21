709c4408e6e50ef76ddb4da7e1a78e9a081b50ee
Ajinkya <ap@gmail.com>
2025-04-21 16:50:24 +0530

    Smartphone manCanvas 5.0

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
       {/* ManCanvas with conditional size */}
       <div
         className={`absolute w-full h-full ${
-          isMobile ? "scale-[1.25] top-10" : ""
+          isMobile ? "scale-[0.6] top-28" : "top-0"
         }`}
       >
         <ManCanvas />
